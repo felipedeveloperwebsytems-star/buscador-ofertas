fc39a36771621f57d09e5be9e5182352493acf1d
felipetechgreen <felipetechgreen@gmail.com>
2026-02-08 04:30:47 -0300

    Fix: Ativando rastreio de afiliado dryfour-20

--- linksAfiliados.js
+++ linksAfiliados.js
@@ -3,33 +3,6 @@ const linksDeAfiliados = {
         "Amazon.com.br": "https://www.amazon.com.br/?tag=dryfour-20"
     },
     produtos: [
-        // --- ORIGINAIS (Mantidos) ---
-        {
-            keyword: "Galaxy A55",
-            title: "Samsung Galaxy A55 5G 128GB - Azul Escuro",
-            price: "R$ 1.899,00",
-            store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/61c+uQLo3XL._AC_SY300_SX300_QL70_ML2_.jpg",
-            link: "https://www.amazon.com.br/Samsung-Traseira-Nightography-Apagador-objetos/dp/B0CX26T3VL?tag=dryfour-20"
-        },
-        {
-            keyword: "Echo Dot 5",
-            title: "Echo Dot 5ª Geração | Som vibrante e Alexa",
-            price: "R$ 429,00",
-            store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/51SBO6p2v9L._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/echo-dot-5a-geracao-preta/dp/B09B8V1LZ3?tag=dryfour-20"
-        },
-        {
-            keyword: "Drone",
-            title: "Drone DJI Mini 4K - Ultra HD Vídeo",
-            price: "R$ 2.990,00",
-            store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/51S4CAn06VL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B0D1P3K9V6?tag=dryfour-20"
-        },
-
-        // --- NOVOS: CAMPEÕES DE VENDA ---
         {
             keyword: "Kindle",
             title: "Kindle 11ª Geração - Mais leve e com tela de 300 ppi",
@@ -40,155 +13,163 @@ const linksDeAfiliados = {
         },
         {
             keyword: "Fire TV",
-            title: "Fire TV Stick Full HD com Controle Remoto por Voz Alexa",
-            price: "R$ 284,00",
+            title: "Novo Fire TV Stick HD com Controle Remoto por Voz Alexa",
+            price: "R$ 284,05",
             store: "Amazon.com.br",
             thumbnail: "https://m.media-amazon.com/images/I/41uAlre7LSL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B08C1W5N87?tag=dryfour-20"
+            link: "https://www.amazon.com.br/dp/B0CQMT33WX?tag=dryfour-20"
         },
         {
-            keyword: "PlayStation 5",
-            title: "Console PlayStation 5 Slim - Edição Digital",
-            price: "R$ 3.750,00",
+            keyword: "Echo Dot",
+            title: "Echo Dot 5ª Geração - Cor Preta",
+            price: "R$ 426,55",
             store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/41OInqSshfL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B0CLT5Y6NQ?tag=dryfour-20"
+            thumbnail: "https://m.media-amazon.com/images/I/51SBO6p2v9L._AC_SL1000_.jpg",
+            link: "https://www.amazon.com.br/dp/B09B8VGCR8?tag=dryfour-20"
         },
         {
             keyword: "iPhone 15",
             title: "Apple iPhone 15 (128 GB) - Preto",
-            price: "R$ 4.899,00",
+            price: "R$ 4.699,00",
             store: "Amazon.com.br",
             thumbnail: "https://m.media-amazon.com/images/I/71v2jVh6nIL._AC_SL1500_.jpg",
-            link: "https://www.amazon.com.br/dp/B0CHX1W1XY?tag=dryfour-20"
+            link: "https://www.amazon.com.br/dp/B0CP6CVJSG?tag=dryfour-20"
+        },
+        {
+            keyword: "iPhone 16",
+            title: "Apple iPhone 16 (128 GB) - Preto",
+            price: "R$ 7.019,10",
+            store: "Amazon.com.br",
+            thumbnail: "https://m.media-amazon.com/images/I/61fS09SsqRL._AC_SL1500_.jpg",
+            link: "https://www.amazon.com.br/dp/B0DJFTJ6LX?tag=dryfour-20"
+        },
+        {
+            keyword: "Galaxy A56",
+            title: "Samsung Galaxy A56 5G - Câmera Tripla e Nightography",
+            price: "R$ 2.499,00",
+            store: "Amazon.com.br",
+            thumbnail: "https://m.media-amazon.com/images/I/61m1hE78GTL._AC_SL1500_.jpg",
+            link: "https://www.amazon.com.br/dp/B0DYVP8SML?tag=dryfour-20"
+        },
+        {
+            keyword: "PlayStation 5",
+            title: "Console PlayStation 5 Slim Edição Digital",
+            price: "R$ 3.719,07",
+            store: "Amazon.com.br",
+            thumbnail: "https://m.media-amazon.com/images/I/41OInqSshfL._AC_SL1000_.jpg",
+            link: "https://www.amazon.com.br/dp/B0FPGF9J2J?tag=dryfour-20"
+        },
+        {
+            keyword: "Redmi Note 14",
+            title: "Smartphone Xiaomi Redmi Note 14 Pro 5G",
+            price: "R$ 1.890,00",
+            store: "Amazon.com.br",
+            thumbnail: "https://m.media-amazon.com/images/I/518v+L+D4FL._AC_SL1000_.jpg",
+            link: "https://www.amazon.com.br/dp/B0DTFLCBCF?tag=dryfour-20"
         },
         {
             keyword: "Air Fryer",
-            title: "Fritadeira Sem Óleo Mondial Family - 4L",
-            price: "R$ 349,00",
+            title: "Fritadeira Air Fryer Forno Mondial - 12L",
+            price: "R$ 679,90",
             store: "Amazon.com.br",
             thumbnail: "https://m.media-amazon.com/images/I/61mG8o-G5tL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B07YF1D5D1?tag=dryfour-20"
-        },
-        {
-            keyword: "Smart Lamp",
-            title: "Lâmpada Inteligente Wi-Fi Positivo Casa Inteligente",
-            price: "R$ 49,00",
-            store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/51R8f8-UAML._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B07Z126W1C?tag=dryfour-20"
-        },
-        {
-            keyword: "Robo Aspirador",
-            title: "Robô Aspirador WAP ROBOT W100 - Varre, Aspira e Passa Pano",
-            price: "R$ 459,00",
-            store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/61vYfB+iUeL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B08G8B87G7?tag=dryfour-20"
-        },
-        {
-            keyword: "Logitech Mouse",
-            title: "Mouse Sem Fio Logitech M280 - Confortável e Preciso",
-            price: "R$ 79,00",
-            store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/51D5S7eT47L._AC_SL1125_.jpg",
-            link: "https://www.amazon.com.br/dp/B00N3BBM0W?tag=dryfour-20"
-        },
-        {
-            keyword: "Redmi Note 13",
-            title: "Xiaomi Redmi Note 13 4G 256GB - 8GB RAM",
-            price: "R$ 1.250,00",
-            store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/518v+L+D4FL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B0CQM8B56C?tag=dryfour-20"
-        },
-        {
-            keyword: "JBL Flip 6",
-            title: "Caixa de Som Bluetooth JBL Flip 6 - À prova d'água",
-            price: "R$ 649,00",
-            store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/61XpSUnG63L._AC_SL1500_.jpg",
-            link: "https://www.amazon.com.br/dp/B09G96T8YY?tag=dryfour-20"
+            link: "https://www.amazon.com.br/dp/B0BZJDB39Z?tag=dryfour-20"
         },
         {
             keyword: "Stanley",
-            title: "Copo Térmico de Cerveja Stanley com Tampa | 473ml",
-            price: "R$ 165,00",
+            title: "Copo Térmico de Cerveja Stanley - 473ml",
+            price: "R$ 152,10",
             store: "Amazon.com.br",
             thumbnail: "https://m.media-amazon.com/images/I/51yVlYq0YyL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B08T6K2ZRT?tag=dryfour-20"
+            link: "https://www.amazon.com.br/dp/B079Y85F2G?tag=dryfour-20"
+        },
+        {
+            keyword: "JBL Flip",
+            title: "Caixa de Som Bluetooth JBL Flip 6 À prova d'água",
+            price: "R$ 629,00",
+            store: "Amazon.com.br",
+            thumbnail: "https://m.media-amazon.com/images/I/61XpSUnG63L._AC_SL1500_.jpg",
+            link: "https://www.amazon.com.br/dp/B0F7T2Y46Y?tag=dryfour-20"
+        },
+        {
+            keyword: "Smart Lamp",
+            title: "Lâmpada Inteligente Elgin Wi-Fi RGB",
+            price: "R$ 39,90",
+            store: "Amazon.com.br",
+            thumbnail: "https://m.media-amazon.com/images/I/51R8f8-UAML._AC_SL1000_.jpg",
+            link: "https://www.amazon.com.br/dp/B0BDZXLS77?tag=dryfour-20"
+        },
+        {
+            keyword: "Robo Aspirador",
+            title: "Robô Aspirador WAP W90 - Automático e Bivolt",
+            price: "R$ 419,90",
+            store: "Amazon.com.br",
+            thumbnail: "https://m.media-amazon.com/images/I/61vYfB+iUeL._AC_SL1000_.jpg",
+            link: "https://www.amazon.com.br/dp/B0B9PSBNYL?tag=dryfour-20"
+        },
+        {
+            keyword: "Mouse Logitech",
+            title: "Mouse Sem Fio Logitech M280 - Ergonomia Ambidestra",
+            price: "R$ 79,90",
+            store: "Amazon.com.br",
+            thumbnail: "https://m.media-amazon.com/images/I/51D5S7eT47L._AC_SL1125_.jpg",
+            link: "https://www.amazon.com.br/dp/B00UC1R80C?tag=dryfour-20"
         },
         {
             keyword: "Cadeira Gamer",
-            title: "Cadeira Gamer Profissional Ergonômica Reclinável",
-            price: "R$ 599,00",
+            title: "Cadeira Gamer ThunderX3 Yama1 Ergonomica",
+            price: "R$ 999,00",
             store: "Amazon.com.br",
             thumbnail: "https://m.media-amazon.com/images/I/61k8f3fA5FL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B097S5XN8G?tag=dryfour-20"
+            link: "https://www.amazon.com.br/dp/B07XM8KGBB?tag=dryfour-20"
         },
         {
-            keyword: "Microfone HyperX",
-            title: "Microfone Gamer HyperX QuadCast S - RGB e Condensador",
-            price: "R$ 899,00",
-            store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/613p8p547dL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B08G8WH434?tag=dryfour-20"
-        },
-        {
-            keyword: "Fone Sony",
-            title: "Fone de Ouvido Sony WH-1000XM5 - Cancelamento de Ruído",
-            price: "R$ 1.999,00",
+            keyword: "Sony WH",
+            title: "Fone Sony WF-1000XM5 com Cancelamento de Ruído",
+            price: "R$ 1.849,00",
             store: "Amazon.com.br",
             thumbnail: "https://m.media-amazon.com/images/I/51f8Bf9WpGL._AC_SL1200_.jpg",
-            link: "https://www.amazon.com.br/dp/B09XS7JWHH?tag=dryfour-20"
+            link: "https://www.amazon.com.br/dp/B0C33XXS56?tag=dryfour-20"
         },
         {
             keyword: "Nintendo Switch",
-            title: "Console Nintendo Switch OLED - Branco",
-            price: "R$ 2.150,00",
+            title: "Console Nintendo Switch OLED 64GB - Branco",
+            price: "R$ 2.199,00",
             store: "Amazon.com.br",
             thumbnail: "https://m.media-amazon.com/images/I/51767o-0oFL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B09NMMK9C6?tag=dryfour-20"
+            link: "https://www.amazon.com.br/dp/B09KMW2N9Z?tag=dryfour-20"
         },
         {
-            keyword: "Cafeteira Nespresso",
-            title: "Cafeteira Nespresso Vertuo Pop - Café Espresso e Caneca",
-            price: "R$ 499,00",
+            keyword: "Nespresso",
+            title: "Cafeteira Nespresso Vertuo Pop - Design Compacto",
+            price: "R$ 449,00",
             store: "Amazon.com.br",
             thumbnail: "https://m.media-amazon.com/images/I/513fD-nCqyL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B0C399S8N4?tag=dryfour-20"
+            link: "https://www.amazon.com.br/dp/B0FST4RPBG?tag=dryfour-20"
         },
         {
-            keyword: "Lego Star Wars",
-            title: "LEGO Star Wars Capacete de Darth Vader",
-            price: "R$ 480,00",
-            store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/81h9h-E9Z5L._AC_SL1500_.jpg",
-            link: "https://www.amazon.com.br/dp/B08GZ2S6KW?tag=dryfour-20"
-        },
-        {
-            keyword: "Galaxy Watch 6",
-            title: "Samsung Galaxy Watch 6 BT 40mm - Grafite",
-            price: "R$ 1.199,00",
-            store: "Amazon.com.br",
-            thumbnail: "https://m.media-amazon.com/images/I/61m1hE78GTL._AC_SL1500_.jpg",
-            link: "https://www.amazon.com.br/dp/B0CC9H5R4M?tag=dryfour-20"
-        },
-        {
-            keyword: "Aspirador de Pó",
-            title: "Aspirador de Pó Vertical Philco 2 em 1 - 1000W",
-            price: "R$ 189,00",
+            keyword: "Aspirador Vertical",
+            title: "Aspirador de Pó Vertical Philco 1450w",
+            price: "R$ 209,90",
             store: "Amazon.com.br",
             thumbnail: "https://m.media-amazon.com/images/I/51-7mB-f6JL._AC_SL1000_.jpg",
-            link: "https://www.amazon.com.br/dp/B07RXYX5K7?tag=dryfour-20"
+            link: "https://www.amazon.com.br/dp/B0C44K7N7G?tag=dryfour-20"
         },
         {
             keyword: "Monitor LG",
             title: "Monitor Gamer LG UltraGear 24' IPS 144Hz 1ms",
-            price: "R$ 899,00",
+            price: "R$ 879,00",
             store: "Amazon.com.br",
             thumbnail: "https://m.media-amazon.com/images/I/71Y8L6O393L._AC_SL1500_.jpg",
-            link: "https://www.amazon.com.br/dp/B0C8V2YHL8?tag=dryfour-20"
+            link: "https://www.amazon.com.br/dp/B0D8DPT7ZY?tag=dryfour-20"
+        },
+        {
+            keyword: "Galaxy Watch",
+            title: "Samsung Galaxy Watch 6 Classic BT 43mm",
+            price: "R$ 1.299,00",
+            store: "Amazon.com.br",
+            thumbnail: "https://m.media-amazon.com/images/I/61m1hE78GTL._AC_SL1500_.jpg",
+            link: "https://www.amazon.com.br/dp/B0CCQP9JB7?tag=dryfour-20"
         }
     ]
 };
